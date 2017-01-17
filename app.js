3a1b887e76a3a4c693b92bd7367df17d8d7fc82f
Rukia Sheikh-mohamed <rukiasheikh-mohamed@macbook-2.local>
2017-01-17 09:03:24 -0600

    changed .data method

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
   console.log('document is ready');
   $('button').on('click', function (){
     //created thisColor variable to specify color when appending to the DOM
-    var thisColor = $(this).attr('data-color');
+    var thisColor = $(this).data('color');
     counter();
     $('.container').append('<div class="color-cube  '+ thisColor + '"></div>');
     $('p').children('span#red').text(red);
